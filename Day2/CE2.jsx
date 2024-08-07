b172905464afe9655b11a60343cd42ead5810a9c
saranishr <142418625+saranishr@users.noreply.github.com>
2024-08-07 17:23:00 +0530

    Update CE2.jsx

--- Day2/CE2.jsx
+++ Day2/CE2.jsx
@@ -1,1 +1,26 @@
-
+import React from 'react';
+function App() {
+  const headingStyle = {
+    color: 'green',
+  };
+  const paragraphStyle = {
+    color: 'darkblue',
+    fontSize: '16px',
+  };
+  const  divStyle = {
+    backgroundColor: 'lightblue',
+    color: 'darkblue',
+    padding: '10px',
+    border: '1px solid blue',
+    borderRadius: '5px',
+  };
+  return (
+    <div style={divStyle}>
+      <h1 style={headingStyle}>Inline Style in JSX Example</h1>
+      <p style={paragraphStyle}>
+        This is a paragraph with inline styles applied.
+      </p>
+    </div>
+  );
+}
+export default App;
